50f428522b4b1da6d710495e064cda4e6b26d37e
Christian V <viray.christianj@gmail.com>
2018-11-06 23:34:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 def prime?(num)
-  num % (2) != 0
-  else 
-    false
+  if num < 2 || num == 0 || num == 1
+    return false 
+ else   
+  (2..num-1).to_a.none? do |i|
+    if num % i == 0
+    end
   end 
 end 
