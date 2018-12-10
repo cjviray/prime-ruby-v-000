67c57e96b7bd231c4716a175eb211a714089936f
Christian V <viray.christianj@gmail.com>
2018-12-10 06:25:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(num)
   if num < 2
     return false
-  end 
+  end
     (2..num-1).each do |i|
       if num % i == 0
         return false
       end
     end 
       return true
-    end 
+    end
